840afe5dbacf9e5433a26b03ecf38068bca60de0
RogueOneEcho <Um9ndWVPbmVFY2hv@pm.me>
2024-12-13 19:26:09 +0000

    feat: make GazelleClientOptions public

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 pub use client::*;
 pub use factory::*;
+pub use options::*;
 pub use schema::*;
 
 mod client;
 #[cfg(test)]
 mod client_tests;
 mod factory;
-#[cfg(test)]
 mod options;
 mod schema;
